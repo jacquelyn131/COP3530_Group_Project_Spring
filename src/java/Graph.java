f421e68a5e3593cab4f90b6c56b0c71d26e3b379
jacquelyn131 <jacquelyns131@gmail.com>
2023-04-03 16:39:28 -0400

    constructor
    - added constructor with no parameters
    - added constructor with List<Vertex> as a parameter
    - added vertexList field

--- src/java/Graph.java
+++ src/java/Graph.java
@@ -1,3 +1,12 @@
 public class Graph {
-    
+    List<Vertex> vertexList; // List of vertices in the graph.
+
+    Graph()
+    {
+        this.vertexList = new ArrayList<Vertex>();
+    }
+    Graph(List<Vertex> vertices)
+    {
+        this.vertexList = vertices;
+    }
 }
