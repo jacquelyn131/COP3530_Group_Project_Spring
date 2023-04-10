919d7da9bd4f22f1e496246bab4b8a8bf8c39064
jacquelyn131 <jacquelyns131@gmail.com>
2023-04-09 22:37:34 -0400

    constructor
    - added a constructor

--- src/java/AdjVertex.java
+++ src/java/AdjVertex.java
@@ -2,4 +2,11 @@
 public class AdjVertex { // Class to store adjacent vertex and weight together.
     public Vertex v;
     public int weight;
+
+    AdjVertex(Vertex v, int w)
+    {
+        this.v = v;
+        this.weight = w;
+    }
+        
 }
